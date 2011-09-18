60bd256deb6a051581010af79456f4a7c10e08ab
Bas1l <Bas1l@d734c9c1-d233-4850-bbe2-26faf33ccd8e>
2011-09-18 15:48:19 +0000

    Tray minimization added; sample windows navigation added.

---
+++ trunk/WindowsExplorerClient/MainWindow.xaml.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.ComponentModel;
+using System.Windows;
+
+namespace WindowsExplorerClient
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private System.Windows.Forms.NotifyIcon _notifyIcon;
+        private WindowState _storedWindowState = WindowState.Normal;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            // initialise code here
+            _notifyIcon = new System.Windows.Forms.NotifyIcon();
+            _notifyIcon.BalloonTipText = "The app has been minimised. Click the tray icon to show.";
+            _notifyIcon.BalloonTipTitle = "The App";
+            _notifyIcon.Text = "The App";
+            _notifyIcon.Icon = Properties.Resources.TrayIcon;
+            _notifyIcon.Click += NotifyIconClick;
+        }
+
+        private void HandleClose(object sender, CancelEventArgs args)
+        {
+            _notifyIcon.Dispose();
+            _notifyIcon = null;
+        }
+
+        private void HandleStateChanged(object sender, EventArgs args)
+        {
+            if (WindowState == WindowState.Minimized)
+            {
+                Hide();
+                if (_notifyIcon != null)
+                {
+                    _notifyIcon.ShowBalloonTip(2000);
+                }
+            }
+            else
+            {
+                _storedWindowState = WindowState;
+            }
+        }
+
+        private void HandleIsVisibleChanged(object sender, DependencyPropertyChangedEventArgs args)
+        {
+            CheckTrayIcon();
+        }
+
+        private void NotifyIconClick(object sender, EventArgs e)
+        {
+            Show();
+            WindowState = _storedWindowState;
+        }
+
+        private void CheckTrayIcon()
+        {
+            ShowTrayIcon(!IsVisible);
+        }
+
+        private void ShowTrayIcon(bool visible)
+        {
+            if (_notifyIcon != null)
+            {
+                _notifyIcon.Visible = visible;
+            }
+        }
+    }
+}
