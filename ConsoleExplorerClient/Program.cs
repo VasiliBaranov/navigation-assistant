81054bb8e3a383a259e8889d88cc35f9bd120c3a
Bas1l <Bas1l@d734c9c1-d233-4850-bbe2-26faf33ccd8e>
2011-09-18 15:48:19 +0000

    Tray minimization added; sample windows navigation added.

---
+++ ConsoleExplorerClient/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Threading;
+using System.Runtime.InteropServices;
+
+namespace ConsoleExplorerClient
+{
+    internal class Program
+    {
+        // Declare external functions.
+        [DllImport("user32.dll")]
+        private static extern IntPtr GetForegroundWindow();
+
+        [DllImport("user32.dll")]
+        private static extern int GetWindowText(IntPtr hWnd, StringBuilder text, int count);
+
+        private static void Main(string[] args)
+        {
+            SHDocVw.ShellWindows shellWindows = new SHDocVw.ShellWindowsClass();
+
+            string filename;
+
+            foreach (SHDocVw.InternetExplorer ie in shellWindows)
+            {
+                filename = Path.GetFileNameWithoutExtension(ie.FullName).ToLower();
+
+                if (filename.Equals("iexplore"))
+                {
+                    Console.WriteLine("Web Site   : {0}", ie.LocationURL);
+                }
+
+                if (filename.Equals("explorer"))
+                {
+                    Console.WriteLine("Hard Drive : {0}", ie.LocationURL);
+
+                    ie.Navigate("file:///e:\\temp\\my folder\\");
+
+                    Thread.Sleep(1000);
+                    Console.WriteLine("Hard Drive : {0}", ie.LocationURL);
+                }
+            }
+
+            int chars = 256;
+            StringBuilder buff = new StringBuilder(chars);
+
+            // Obtain the handle of the active window.
+            IntPtr handle = GetForegroundWindow();
+
+            // Update the controls.
+            if (GetWindowText(handle, buff, chars) > 0)
+            {
+                Console.WriteLine(buff.ToString());
+                Console.WriteLine(handle.ToString());
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Core/Services/Implementation/CachedFileSystemParser.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web.Caching;
+using Core.Model;
+
+namespace Core.Services.Implementation
+{
+    public class CachedFileSystemParser : IFileSystemParser
+    {
+        #region Fields
+
+        private const string CacheKey = "Folders";
+
+        private readonly IFileSystemParser _fileSystemParser;
+
+        private readonly Cache _cache;
+
+        private readonly TimeSpan _cacheExpiration;
+
+        #endregion
+
+        #region Constructors
+
+        public CachedFileSystemParser(IFileSystemParser fileSystemParser)
+        {
+            _fileSystemParser = fileSystemParser;
+            _cache = new Cache();
+            _cacheExpiration = new TimeSpan(0, 0, 10, 0); //10 minutes
+        }
+
+        #endregion
+
+        #region Public Methods
+
+        public List<FileSystemItem> GetFolders(string rootPath)
+        {
+            List<FileSystemItem> folders = _cache.Get(CacheKey) as List<FileSystemItem>;
+
+            if (folders == null)
+            {
+                folders = _fileSystemParser.GetFolders(rootPath);
+
+                DateTime absoluteExpiration = DateTime.Now.Add(_cacheExpiration);
+                _cache.Add(CacheKey, folders, null, absoluteExpiration, Cache.NoSlidingExpiration, CacheItemPriority.Normal, null);
+            }
+
+            return folders;
+        }
+
+        #endregion
+    }
+}
