02e7825742e228b328b2273cbf2008dfcddc144e
Bas1l <Bas1l@d734c9c1-d233-4850-bbe2-26faf33ccd8e>
2011-09-18 19:42:56 +0000

    Sample markup and viewmodel added.

---
+++ trunk/WindowsExplorerClient/ViewModel.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+
+namespace WindowsExplorerClient
+{
+    public class ViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private string _searchText;
+
+        private readonly ObservableCollection<string> _matches = new ObservableCollection<string>();
+
+        public string SearchText
+        {
+            get
+            {
+                return _searchText;
+            }
+            set
+            {
+                _searchText = value;
+
+                _matches.Clear();
+                _matches.Add(_searchText + "1");
+                _matches.Add(_searchText + "2");
+
+                PropertyChanged(this, new PropertyChangedEventArgs("Matches"));
+                PropertyChanged(this, new PropertyChangedEventArgs("SearchText"));
+            }
+        }
+
+        public ObservableCollection<string> Matches
+        {
+            get
+            {
+                return _matches;
+            }
+        }
+    }
+}
