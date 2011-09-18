e64c6f43f4aa42de1b586dddcdafcdc723940f74
Bas1l <Bas1l@d734c9c1-d233-4850-bbe2-26faf33ccd8e>
2011-09-18 23:05:04 +0000

    Correct tray icon behaviour implemented, binding delay added.

--- trunk/WindowsExplorerClient/ViewModel.cs
+++ trunk/WindowsExplorerClient/ViewModel.cs
@@ -2,10 +2,12 @@
 using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Globalization;
+using Core;
 using Core.Model;
 using Core.Services;
 using Core.Services.Implementation;
 using System.Linq;
+using Timer = System.Timers.Timer;
 
 namespace WindowsExplorerClient
 {
@@ -17,15 +19,23 @@ namespace WindowsExplorerClient
 
         private readonly INavigationAssistant _navigationAssistant;
 
-        private ObservableCollection<string> _matches = new ObservableCollection<string>();
+        private readonly Timer _delayTimer;
+
+        private ObservableCollection<string> _matches = new ObservableCollection<string> { InitialMatchesMessage };
 
         private string _searchText;
 
         public event PropertyChangedEventHandler PropertyChanged;
 
+        private const int DelayInMilliseconds = 200;
+
         private const int MaxMatchesToDisplay = 20;
 
         private const string TooManyMatchesText = "Too many matches...";
+
+        private const string NoMatchesFound = "No matches found";
+
+        private const string InitialMatchesMessage = "Please type folder name";
 
         #endregion
 
@@ -35,6 +45,19 @@ namespace WindowsExplorerClient
         {
             IFileSystemParser fileSystemParser = new CachedFileSystemParser(new FileSystemParser(), new CacheSerializer(@"e:\temp\Cache.txt"));
             _navigationAssistant = new NavigationAssistant(fileSystemParser, new MatchSearcher());
+
+            _delayTimer = new Timer();
+            _delayTimer.AutoReset = false;
+            _delayTimer.Interval = DelayInMilliseconds;
+            _delayTimer.Elapsed += HandleDelayElapsed;
+        }
+
+        private void HandleDelayElapsed(object sender, System.Timers.ElapsedEventArgs e)
+        {
+            _matches = new ObservableCollection<string>(GetMatchRepresentations(_searchText));
+
+            PropertyChanged(this, new PropertyChangedEventArgs("Matches"));
+            PropertyChanged(this, new PropertyChangedEventArgs("SearchText"));
         }
 
         #endregion
@@ -51,10 +74,12 @@ namespace WindowsExplorerClient
             {
                 _searchText = value;
 
-                _matches = new ObservableCollection<string>(GetMatchRepresentations(_searchText));
+                //To avoid too frequent updates we run a timer to take a pause.
+                //If search text is not updated during this pause, matches will be rendered.
 
-                PropertyChanged(this, new PropertyChangedEventArgs("Matches"));
-                PropertyChanged(this, new PropertyChangedEventArgs("SearchText"));
+                //Need to stop the execution from the previous setter.
+                _delayTimer.Stop();
+                _delayTimer.Start();
             }
         }
 
@@ -73,6 +98,11 @@ namespace WindowsExplorerClient
         private List<string> GetMatchRepresentations(string searchText)
         {
             List<MatchedFileSystemItem> folderMatches = _navigationAssistant.GetFolderMatches(_rootFolders, searchText);
+            if (Utilities.IsNullOrEmpty(folderMatches))
+            {
+                return new List<string>{NoMatchesFound};
+            }
+
             List<string> matchRepresentations = folderMatches
                 .Take(MaxMatchesToDisplay)
                 .OrderBy(m=>m.ItemPath.Length)
