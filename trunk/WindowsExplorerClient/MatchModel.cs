c79bc890f09cacafb9c92975555cc8c60a0c401e
Bas1l <Bas1l@d734c9c1-d233-4850-bbe2-26faf33ccd8e>
2011-09-19 20:44:39 +0000

    UI improved, windows explorer interaction updated.

---
+++ trunk/WindowsExplorerClient/MatchModel.cs
@@ -0,0 +1,15 @@
+﻿namespace WindowsExplorerClient
+{
+    public class MatchModel
+    {
+        public string Text { get; set; }
+
+        public string Path { get; set; }
+
+        public MatchModel(string text, string path)
+        {
+            Text = text;
+            Path = path;
+        }
+    }
+}
