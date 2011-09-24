9daa8618670f1f0ed1e029b9eb12b894efce17c3
Bas1l <Bas1l@d734c9c1-d233-4850-bbe2-26faf33ccd8e>
2011-09-24 17:57:58 +0000

    Max listbox height determined automatically.

---
+++ trunk/WindowsExplorerClient/PresentationServices/Implementations/PresentationService.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Windows;
+using System.Windows.Forms;
+using System.Windows.Interop;
+using Core.Utilities;
+using WindowsExplorerClient.ViewModel;
+using Application = System.Windows.Application;
+
+namespace WindowsExplorerClient.PresentationServices.Implementations
+{
+    public class PresentationService : IPresentationService
+    {
+        private const double MaxScreenFillingRatio = 0.7;
+
+        public MatchModel MoveSelectionUp(ObservableCollection<MatchModel> matches, MatchModel selectedMatch)
+        {
+            if (Utility.IsNullOrEmpty(matches))
+            {
+                return null;
+            }
+
+            int selectionIndex = matches.IndexOf(selectedMatch);
+            selectionIndex--;
+
+            if (selectionIndex < 0)
+            {
+                selectionIndex = matches.Count - 1;
+            }
+
+            return matches[selectionIndex];
+        }
+
+        public MatchModel MoveSelectionDown(ObservableCollection<MatchModel> matches, MatchModel selectedMatch)
+        {
+            if (Utility.IsNullOrEmpty(matches))
+            {
+                return null;
+            }
+
+            int selectionIndex = matches.IndexOf(selectedMatch);
+            selectionIndex++;
+
+            if (selectionIndex == matches.Count)
+            {
+                selectionIndex = 0;
+            }
+
+            return matches[selectionIndex];
+        }
+
+        public double GetMaxMatchesListHeight(double searchTextBoxTop, double searchTextBoxHeight)
+        {
+            Window currentWindow = Application.Current.MainWindow;
+            double windowTopPosition = currentWindow.Top;
+
+            //Need to do some hacks to handle multiple monitors.
+            //For single monitor System.Windows.SystemParameters.WorkArea or System.Windows.SystemParameters.PrimaryScreenHeight
+            //would have been sufficient
+            //See http://stackoverflow.com/questions/254197/how-can-i-get-the-active-screen-dimensions
+            Screen screen = GetScreen(currentWindow);
+            double screenHeight = screen.WorkingArea.Height;
+
+            double availableHeight = screenHeight - windowTopPosition - searchTextBoxTop - searchTextBoxHeight;
+            return availableHeight*MaxScreenFillingRatio;
+        }
+
+        private static Screen GetScreen(Window window)
+        {
+            return Screen.FromHandle(new WindowInteropHelper(window).Handle);
+        }
+
+    }
+}
