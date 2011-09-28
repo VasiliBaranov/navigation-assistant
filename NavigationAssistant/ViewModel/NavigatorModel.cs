918b435a3e11a77647fbc7a74971205dfba758fa
vasili.baranov <vasili.baranov@googlemail.com>
2011-09-28 22:48:37 +0000

    Basic settings model implemented.

---
+++ NavigationAssistant/ViewModel/NavigatorModel.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.ComponentModel;
+using NavigationAssistant.PresentationModel;
+
+namespace NavigationAssistant.ViewModel
+{
+    public class NavigatorModel : INotifyPropertyChanged
+    {
+        #region Fields
+
+        private string _name;
+
+        private Navigators _type;
+
+        private bool _isSelected;
+
+        private bool _isEnabled = true;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public event EventHandler IsSelectedChanged;
+
+        #endregion
+
+        #region Constructors
+
+        public NavigatorModel(string name, Navigators type)
+        {
+            _name = name;
+            _type = type;
+        }
+
+        #endregion
+
+        #region Properties
+
+        public string Name
+        {
+            get
+            {
+                return _name;
+            }
+            set
+            {
+                _name = value;
+                OnPropertyChanged("Name");
+            }
+        }
+
+        public Navigators Type
+        {
+            get
+            {
+                return _type;
+            }
+            set
+            {
+                _type = value;
+                OnPropertyChanged("Type");
+            }
+        }
+
+        public bool IsSelected
+        {
+            get
+            {
+                return _isSelected;
+            }
+            set
+            {
+                _isSelected = value;
+                OnPropertyChanged("IsSelected");
+                OnIsSelectedChanged();
+            }
+        }
+
+        public bool IsEnabled
+        {
+            get
+            {
+                return _isEnabled;
+            }
+            set
+            {
+                _isEnabled = value;
+                OnPropertyChanged("IsEnabled");
+            }
+        }
+
+        #endregion
+
+        #region Non Public Methods
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+        protected virtual void OnIsSelectedChanged()
+        {
+            if (IsSelectedChanged != null)
+            {
+                IsSelectedChanged(this, new EventArgs());
+            }
+        }
+
+        #endregion
+    }
+}
--- NavigationAssistant/ViewModel/SettingsModel.cs
+++ NavigationAssistant/ViewModel/SettingsModel.cs
@@ -1,33 +1,182 @@
-﻿using System.Collections.Generic;
-using System.Windows.Forms;
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
 using NavigationAssistant.PresentationModel;
 using NavigationAssistant.PresentationServices;
 using NavigationAssistant.PresentationServices.Implementations;
 
 namespace NavigationAssistant.ViewModel
 {
-    public class SettingsModel
+    public class SettingsModel : INotifyPropertyChanged
     {
+        #region Fields
+
+        private Settings _settings;
+
+        private ObservableCollection<NavigatorModel> _primaryNavigatorModels;
+
+        private ObservableCollection<NavigatorModel> _supportedNavigatorModels;
+
+        private bool _totalCommanderEnabled;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        #endregion
+
+        #region Constructors
+
         public SettingsModel()
         {
             ISettingsSerializer settingsSerializer = new SettingsSerializer();
-            Settings settings = settingsSerializer.Deserialize();
+            _settings = settingsSerializer.Deserialize();
 
-            if (settings == null)
+            _supportedNavigatorModels = CreateSupportedNavigatorModels(_settings);
+
+            //Can not use the same objects, as their properties may be changed independently.
+            _primaryNavigatorModels = CreatePrimaryNavigatorModels(_settings);
+        }
+
+        #endregion
+
+        #region Properties
+
+        public ObservableCollection<NavigatorModel> PrimaryNavigatorModels
+        {
+            get
             {
-                settings = new Settings();
+                return _primaryNavigatorModels;
+            }
+            set
+            {
+                _primaryNavigatorModels = value;
+                OnPropertyChanged("PrimaryNavigatorModels");
+            }
+        }
+
+        public ObservableCollection<NavigatorModel> SupportedNavigatorModels
+        {
+            get
+            {
+                return _supportedNavigatorModels;
+            }
+            set
+            {
+                _supportedNavigatorModels = value;
+                OnPropertyChanged("SupportedNavigatorModels");
+            }
+        }
+
+        public string TotalCommanderPath
+        {
+            get
+            {
+                return _settings.TotalCommanderPath;
+            }
+            set
+            {
+                _settings.TotalCommanderPath = value;
+                OnPropertyChanged("TotalCommanderPath");
+            }
+        }
+
+        public bool TotalCommanderEnabled
+        {
+            get
+            {
+                return _totalCommanderEnabled;
+            }
+            set
+            {
+                _totalCommanderEnabled = value;
+                OnPropertyChanged("TotalCommanderEnabled");
+            }
+        }
+
+        #endregion
+
+        #region Non Public Methods
+
+        private ObservableCollection<NavigatorModel> CreateSupportedNavigatorModels(Settings settings)
+        {
+            ObservableCollection<NavigatorModel> supportedNavigatorModels = CreateNavigatorModels();
+
+            NavigatorModel totalCommanderNavigatorModel = supportedNavigatorModels.First(sn => sn.Type == Navigators.TotalCommander);
+            totalCommanderNavigatorModel.IsSelectedChanged += HandleTotalCommanderSupportChanged;
+
+            foreach (NavigatorModel navigatorModel in supportedNavigatorModels)
+            {
+                navigatorModel.IsSelected = settings.SupportedNavigators.Contains(navigatorModel.Type);
             }
 
-            settings.FoldersToParse = new List<string> { "E:\\", "D:\\" };
-            settings.IncludeHiddenFolders = true;
-            settings.PrimaryNavigator = Navigators.WindowsExplorer;
-            settings.SupportedNavigators = new List<Navigators> { Navigators.TotalCommander };
-            settings.CacheUpdateIntervalInSeconds = 200;
-            settings.CacheFolder = Application.LocalUserAppDataPath;
-
-            //settingsSerializer.Serialize(settings);
-
-            MessageBox.Show("Settings screen!");
+            return supportedNavigatorModels;
         }
+
+        private void HandleTotalCommanderSupportChanged(object sender, System.EventArgs e)
+        {
+            NavigatorModel totalCommanderNavigatorModel = sender as NavigatorModel;
+            if (totalCommanderNavigatorModel == null)
+            {
+                return;
+            }
+
+            TotalCommanderEnabled = totalCommanderNavigatorModel.IsSelected;
+        }
+
+        private ObservableCollection<NavigatorModel> CreatePrimaryNavigatorModels(Settings settings)
+        {
+            ObservableCollection<NavigatorModel> primaryNavigatorModels = CreateNavigatorModels();
+
+            foreach (NavigatorModel navigator in primaryNavigatorModels)
+            {
+                navigator.IsSelectedChanged += HandlePrimaryNavigatorChanged;
+            }
+
+            NavigatorModel primaryNavigator = primaryNavigatorModels.First(pn => pn.Type == settings.PrimaryNavigator);
+            primaryNavigator.IsSelected = true;
+
+            return primaryNavigatorModels;
+        }
+
+        private ObservableCollection<NavigatorModel> CreateNavigatorModels()
+        {
+            ObservableCollection<NavigatorModel> navigators = new ObservableCollection<NavigatorModel>
+                              {
+                                  new NavigatorModel("Windows Explorer", Navigators.WindowsExplorer),
+                                  new NavigatorModel("Total Commander", Navigators.TotalCommander),
+                              };
+
+            return navigators;
+        }
+
+        private void HandlePrimaryNavigatorChanged(object sender, System.EventArgs e)
+        {
+            NavigatorModel primaryNavigatorModel = sender as NavigatorModel;
+            if (primaryNavigatorModel == null)
+            {
+                return;
+            }
+
+            NavigatorModel supportedNavigatorModel = _supportedNavigatorModels.First(sn => sn.Type == primaryNavigatorModel.Type);
+
+            if (primaryNavigatorModel.IsSelected)
+            {
+                supportedNavigatorModel.IsSelected = true;
+                supportedNavigatorModel.IsEnabled = false;
+            }
+            else
+            {
+                supportedNavigatorModel.IsEnabled = true;
+            }
+        }
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+        #endregion
     }
 }
