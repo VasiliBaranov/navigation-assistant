4b3a1a332aa2f4b7d200a492552dd1bf8df8f378
vasili.baranov <vasili.baranov@googlemail.com>
2011-10-01 21:45:52 +0000

    Key setting layout improved.

--- Core/HookManager/HookManager.KeyMapper.cs
+++ Core/HookManager/HookManager.KeyMapper.cs
@@ -4,8 +4,15 @@ namespace Core.HookManager
 {
     public static class KeyMapper
     {
-        //http://msdn.microsoft.com/en-us/library/windows/desktop/dd375731%28v=vs.85%29.aspx
-        //http://msdn.microsoft.com/en-us/library/system.windows.forms.keys.aspx
+        /// <summary>
+        /// Maps the WinApi virtual key code to the .Net Keys enumeration.
+        /// </summary>
+        /// <param name="virtualKeyCode">The WinApi virtual key code.</param>
+        /// <returns></returns>
+        /// <remarks>
+        /// For WinApi virtual key codes refer to http://msdn.microsoft.com/en-us/library/windows/desktop/dd375731%28v=vs.85%29.aspx.
+        /// For System.Windows.Forms.Keys refer to http://msdn.microsoft.com/en-us/library/system.windows.forms.keys.aspx
+        /// </remarks>
         public static Keys GetKey(int virtualKeyCode)
         {
             if (virtualKeyCode == 164)
