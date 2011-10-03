8e5ddb7941a437732bb50fe821c26e58dfada8bd
vasili.baranov <vasili.baranov@googlemail.com>
2011-10-03 15:26:58 +0000

    Windows and controls moved to View folder.

---
+++ NavigationAssistant/Views/AdvancedSettingsControl.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace NavigationAssistant.Views
+{
+    /// <summary>
+    /// Interaction logic for AdvancedSettingsControl.xaml
+    /// </summary>
+    public partial class AdvancedSettingsControl : UserControl
+    {
+        public AdvancedSettingsControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ NavigationAssistant/Views/BasicSettingsControl.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace NavigationAssistant.Views
+{
+    /// <summary>
+    /// Interaction logic for BasicSettingsControl.xaml
+    /// </summary>
+    public partial class BasicSettingsControl : UserControl
+    {
+        public BasicSettingsControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
