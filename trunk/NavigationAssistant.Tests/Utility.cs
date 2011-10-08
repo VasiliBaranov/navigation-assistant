5cb8f84fc0adc03405a074ff54b249e789e6798b
vasili.baranov@googlemail.com <vasili.baranov@googlemail.com@d734c9c1-d233-4850-bbe2-26faf33ccd8e>
2011-10-08 13:21:03 +0000

    FileSystemParserTests added.

---
+++ trunk/NavigationAssistant.Tests/Utility.cs
@@ -0,0 +1,31 @@
+﻿using System.IO;
+
+namespace NavigationAssistant.Tests
+{
+    public static class Utility
+    {
+        public static void MakeFolderHidden(string path)
+        {
+            DirectoryInfo folder = new DirectoryInfo(path);
+            folder.Attributes = folder.Attributes | FileAttributes.Hidden;
+        }
+
+        public static void MakeFolderVisible(string path)
+        {
+            DirectoryInfo folder = new DirectoryInfo(path);
+            folder.Attributes = folder.Attributes & ~FileAttributes.Hidden;
+        }
+
+        public static void MakeFileHidden(string path)
+        {
+            FileInfo file = new FileInfo(path);
+            file.Attributes = file.Attributes | FileAttributes.Hidden;
+        }
+
+        public static void MakeFileVisible(string path)
+        {
+            FileInfo folder = new FileInfo(path);
+            folder.Attributes = folder.Attributes & ~FileAttributes.Hidden;
+        }
+    }
+}
