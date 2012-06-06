8abe73db26a06dadb5b9aaa8ea8ce100fa7becbb
vasili.baranov <vasili.baranov@gmail.com>
2012-06-06 17:25:39 +0000

    1.0.0.173 release prepared.

--- NavigationAssistant/Properties/AssemblyInfo.cs
+++ NavigationAssistant/Properties/AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System.Windows;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("NavigationAssistant")]
-[assembly: AssemblyCopyright("Copyright © 2011 Vasili Baranov")]
+[assembly: AssemblyCopyright("Copyright © 2012 Vasili Baranov")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -51,5 +51,5 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.152")]
-[assembly: AssemblyFileVersion("1.0.0.152")]
+[assembly: AssemblyVersion("1.0.0.173")]
+[assembly: AssemblyFileVersion("1.0.0.173")]
